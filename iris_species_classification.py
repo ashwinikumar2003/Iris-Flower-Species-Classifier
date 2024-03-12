5058ede574f81d397a5b763b8de71fcf1244947b
Ashwini Kumar <134918219+ashwinikumar2003@users.noreply.github.com>
2024-03-13 01:11:44 +0530

    Update iris_species_classification.py

--- iris_species_classification.py
+++ iris_species_classification.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-import sklearn
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
